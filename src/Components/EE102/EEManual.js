e688b469c7d9c2a37bb8f5918deff36477cf25b5
Anurag Deo <anuragdeo03@gmail.com>
2022-05-04 07:42:12 +0530

    Changed some links in EE

--- src/Components/EE102/EEManual.js
+++ src/Components/EE102/EEManual.js
@@ -9,20 +9,20 @@ const EEManual = () => {
             <Zoom in timeout={2000}>
                 <Box className="flex-container" display="flex" flexDirection="row" alignItems="stretch" padding={1} style={{ justifyContent: 'space-around' }}>
                     <ImgMediaCard label="Lab 1" viewUrl="https://drive.google.com/file/d/121wtx0Rgn64yv2QZdDvShX0waTVqobQ-/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=121wtx0Rgn64yv2QZdDvShX0waTVqobQ-" />
-                    <ImgMediaCard label="Lab 2" viewUrl="https://drive.google.com/file/d/1X1H-_DCGmeITQgEWuUVdAaTeKkAq_Plt/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=t/oX1HmeITQgload&id=1X1H-_DCGmeITQgEWuUVdAaTeKkAq_Plt" />
-                    <ImgMediaCard label="Lab 3" viewUrl="https://drive.google.com/file/d/1GdGPbH1rxsUw_7mUP4FTfq2Oj34IsfRu/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=up4ftoGdGxsUw_7mUPload&id=1GdGPbH1rxsUw_7mUP4FTfq2Oj34IsfRu" />
-                    <ImgMediaCard label="Lab 4" viewUrl="https://drive.google.com/file/d/1He15gLMYNLIIOwIF3aUSYKpcltJGi_io/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?expxpt. Nort=if3.xpt. NoHe1NLIIOwIFlxpt. Noad&id=1He15gLMYNLIIOwIF3aUSYKpcltJGi_io" />
-                    <ImgMediaCard label="Lab 5" viewUrl="https://drive.google.com/file/d/1y2vNKRfOfoMNB_PyMxdbMzcF7yfL_WqA/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=moy2vfoMNBload&id=1y2vNKRfOfoMNB_PyMxdbMzcF7yfL_WqA" />
+                    <ImgMediaCard label="Lab 2" viewUrl="https://drive.google.com/file/d/1X1H-_DCGmeITQgEWuUVdAaTeKkAq_Plt/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=1X1H-_DCGmeITQgEWuUVdAaTeKkAq_Plt" />
+                    <ImgMediaCard label="Lab 3" viewUrl="https://drive.google.com/file/d/1GdGPbH1rxsUw_7mUP4FTfq2Oj34IsfRu/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=1GdGPbH1rxsUw_7mUP4FTfq2Oj34IsfRu" />
+                    <ImgMediaCard label="Lab 4" viewUrl="https://drive.google.com/file/d/1He15gLMYNLIIOwIF3aUSYKpcltJGi_io/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?expxpt=download&id=1He15gLMYNLIIOwIF3aUSYKpcltJGi_io" />
+                    <ImgMediaCard label="Lab 5" viewUrl="https://drive.google.com/file/d/1y2vNKRfOfoMNB_PyMxdbMzcF7yfL_WqA/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=1y2vNKRfOfoMNB_PyMxdbMzcF7yfL_WqA" />
                 </Box>
             </Zoom>
 
             <Zoom in timeout={2000}>
                 <Box className="flex-container" display="flex" flexDirection="row" alignItems="stretch" padding={1} style={{ justifyContent: 'space-around' }}>
-                    <ImgMediaCard label="Lab 6" viewUrl="https://drive.google.com/file/d/18fjHOac0LoHDyofIpqm7YdnwIwqnst5X/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=hdofjLoHDyofIpqmload&id=18fjHOac0LoHDyofIpqm7YdnwIwqnst5X" />
-                    <ImgMediaCard label="Lab 7" viewUrl="https://drive.google.com/file/d/1H8I5XARrcnTFMx_0eNqwTd2w_EPy4HZD/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=n.oH8IcnTFMx_0eNqwTdload&id=1H8I5XARrcnTFMx_0eNqwTd2w_EPy4HZD" />
-                    <ImgMediaCard label="Lab 8" viewUrl="https://drive.google.com/file/d/1xhkJXy69560NmwQ6Q0b8yrG3vgArXIbT/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=8/oxhk560NmwQ6Qload&id=1xhkJXy69560NmwQ6Q0b8yrG3vgArXIbT" />
-                    <ImgMediaCard label="Lab 9" viewUrl="https://drive.google.com/file/d/1mIDfcUbsGzn6IPCm4afTVctCFJ_kEYYV/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=6ipc/omIDGznload&id=1mIDfcUbsGzn6IPCm4afTVctCFJ_kEYYV" />
-                    <ImgMediaCard label="Lab 10" viewUrl="https://drive.google.com/file/d/15AiL5woZ4gy125E_Qi0RlLJoGLjiE8Tr/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=0r/oAi4gy125Eload&id=15AiL5woZ4gy125E_Qi0RlLJoGLjiE8Tr" />
+                    <ImgMediaCard label="Lab 6" viewUrl="https://drive.google.com/file/d/18fjHOac0LoHDyofIpqm7YdnwIwqnst5X/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=18fjHOac0LoHDyofIpqm7YdnwIwqnst5X" />
+                    <ImgMediaCard label="Lab 7" viewUrl="https://drive.google.com/file/d/1H8I5XARrcnTFMx_0eNqwTd2w_EPy4HZD/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=1H8I5XARrcnTFMx_0eNqwTd2w_EPy4HZD" />
+                    <ImgMediaCard label="Lab 8" viewUrl="https://drive.google.com/file/d/1xhkJXy69560NmwQ6Q0b8yrG3vgArXIbT/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=1xhkJXy69560NmwQ6Q0b8yrG3vgArXIbT" />
+                    <ImgMediaCard label="Lab 9" viewUrl="https://drive.google.com/file/d/1mIDfcUbsGzn6IPCm4afTVctCFJ_kEYYV/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=1mIDfcUbsGzn6IPCm4afTVctCFJ_kEYYV" />
+                    <ImgMediaCard label="Lab 10" viewUrl="https://drive.google.com/file/d/15AiL5woZ4gy125E_Qi0RlLJoGLjiE8Tr/view?usp=drivesdk" downloadUrl="https://drive.google.com/uc?export=download&id=15AiL5woZ4gy125E_Qi0RlLJoGLjiE8Tr" />
                 </Box>
             </Zoom>
 
